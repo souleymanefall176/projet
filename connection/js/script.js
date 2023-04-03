7ffb5e1c5a71af609e66914d55d1f9e100862e3f
JulesBest176 <souleymanefall176@gmail.com>
2023-04-03 01:19:34 +0000

    page de connection rs

---
+++ connection/js/script.js
@@ -0,0 +1,16 @@
+/*typing animation */
+/*var typed = new Typed(".typing", {
+    strings: ["", "To Our New Website..."],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+})*/
+const logregBox = document.querySelector(".logreg-box");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+registerLink.addEventListener('click', () => {
+    logregBox.classList.add('active');
+});
+loginLink.addEventListener('click', () => {
+    logregBox.classList.remove('active');
+})
